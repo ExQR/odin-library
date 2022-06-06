a3d2f61ab2ea90ffdbd8560dc6b81107deff0890
ExQRFx <exqr.fx@gmail.com>
2022-06-06 21:41:23 +0500

    get data from form

--- script.js
+++ script.js
@@ -5,26 +5,36 @@ function Book (title, author, pages, read) {
     this.title = title;
     this.pages = pages;
     this.read = read;
-    this.info = function () {
-        return title + ' by ' + author + ', ' + pages + ' pages, ' + read;
-    }
-}
-
-function addBookToLibrary() {
-    myLibrary.push(book);
-}
-
-const addBtn = document.querySelector('#add');
-addBtn.addEventListener('click', (e) => {addBookToLibrary});
+};
 
 const readBtn = document.querySelector('#read');
 readBtn.addEventListener('click', (e) => {
-        if (readBtn.value == "off") {
-            readBtn.value = "on";
+        if (readBtn.value == "false") {
+            readBtn.value = "true";
             readBtn.style = "background-color: rgb(25, 120, 200); transition: 1s"
         } else { 
-            readBtn.value = "off";
+            readBtn.value = "false";
             readBtn.style = "background-color: rgb(194,194,194); transition: 1s"
         }
     }
 );
+
+const submitBtn = document.querySelector('#add');
+
+submitBtn.addEventListener('click', (e) => {
+    let form = document.querySelector('#inputform');
+    let formData = new FormData(form);
+    let book = new Book(formData.get('title'), formData.get('author'), formData.get('pages'), readBtn.value);
+    myLibrary.push(book);
+    addCard();
+    form.reset();
+    readBtn.value = 'false';
+    readBtn.style = "background-color: rgb(194,194,194); transition: 1s"
+    
+} );
+
+function addCard() {
+    const bookshelf = document.querySelector('.bookshelf');
+    let card = document.createElement('div');
+    bookshelf.appendChild(card);
+}
